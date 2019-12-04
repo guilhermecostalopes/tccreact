697d980e14612990801c2801ecdbb6350640b777
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-12-03 23:00:58 -0300

    Ajustes componente pesquisar grupo

--- src/grupos/pesquisar/grupo-pesquisar.js
+++ src/grupos/pesquisar/grupo-pesquisar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
+import ApiGrupoService from '../../service/ApiServiceGrupo';
+
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+
 class GrupoPesquisa extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    ApiGrupoService.todos().then(
+      data => this.setState({grupos: data.lista})
+    );
+  }
+
     render() {
         return (
-          <div className="UsuarioPesquisa">
-              GrupoPesquisa
-          </div>
+          <DataTable value={this.grupos} selection={this.state.selectedCar1} 
+            onSelectionChange={e => this.setState({selectedCar1: e.value})}>
+            <Column field="nome" header="Nome" />
+          </DataTable>
         );
       }
 }
